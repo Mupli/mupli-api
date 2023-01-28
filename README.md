9d0b3fc814122910eef4ff1224dd5b5cf9f2bdf2
przemeksgames <przemeksgames@gmail.com>
2023-01-28 17:58:56 +0100

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+
+
+# API 
+
+
+
+- module name : api
+- path : /api
+- supported files : js, json 
+
+## Ideas 
+
+- adding support for static files generation or static responses
---
+++ mupli-api.js
@@ -0,0 +1,82 @@
+import { FileDetails, FileLoader } from "mupli-core";
+import path from "path";
+
+export class ApiModule {
+    moduleName;
+    _routerPrefix;
+    _routes = {};
+
+    constructor(config = { routerPrefix: undefined, moduleName: undefined }) {
+        this._routerPrefix = config.routerPrefix ?? "/api";
+        this.moduleName = config.moduleName ?? "api";
+    }
+
+    async init(appName) {
+        const me = this;
+        const routes = this._routes;
+        routes[appName] = {};
+
+        const path = "./app/" + appName + "/" + this.moduleName;
+
+        const files = FileLoader.getFiles(path) //
+            .filter((x) => x.is("js") || x.is("json"));
+
+        for (const key in files) {
+            /**
+             * @type {FileDetails}
+             */
+            const fd = files[key];
+
+            if (fd.moduleFilePath.indexOf("/_") < 0) {
+                if (fd.is("js")) {
+                    let actions = await this._getActions(fd);
+
+                    Object.keys(actions).forEach((key) => {
+                        let action = actions[key];
+                        routes[appName][this._routerPrefix + fd.route + key] = action;
+                    });
+                } else if (fd.is("json")) {
+                    const jsonString = FileLoader.load(fd);
+
+                    routes[appName][this._routerPrefix + fd.route] = async (
+                        ctx
+                    ) => {
+                        return jsonString;
+                    };
+                }
+            }
+        }
+    }
+
+    async _getActions(actionFd) {
+        const module = await FileLoader.asObject(actionFd);
+
+        const methodNames = Object.keys(module).filter(
+            (methodName) => !methodName.startsWith("_")
+        );
+
+        if (methodNames.length <= 0)
+            throw new Error(
+                "No '" +
+                    actionFd.name +
+                    "' or 'init' method in file: " +
+                    actionFd.filePath
+            );
+
+        const actions = {};
+        methodNames.forEach((m) => {
+            if (m == "init") {
+                actions[""] = module[m];
+            } else {
+                actions["/" + m] = module[m];
+            }
+        });
+        return actions;
+    }
+
+    routes(appName) {
+        return this._routes[appName];
+    }
+}
+
+export const apiModule = new ApiModule();
